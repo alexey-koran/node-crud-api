f3df979271b252bafcf114609c3c54ad82d9d2c6
Alexey Koran <alexey.koran@gmail.com>
2024-10-22 07:22:47 +0400

    feat: implement `initDatabase`

--- src/db/db.ts
+++ src/db/db.ts
@@ -5,3 +5,21 @@ export const createDatabase = (): Database => {
 
   return database;
 };
+
+interface InitDatabaseReturn {
+  database: Database;
+  updateDatabase: (newDatabase: Database) => void;
+}
+
+export const initDatabase = (): InitDatabaseReturn => {
+  let database = createDatabase();
+
+  const updateDatabase = (newDatabase: Database): void => {
+    database = newDatabase;
+  };
+
+  return {
+    database,
+    updateDatabase,
+  };
+};
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,5 @@
 export { addUser } from './addUser.ts';
-export { createDatabase } from './db.ts';
+export { createDatabase, initDatabase } from './db.ts';
 export { getUserById } from './getUserById.ts';
 export { getUsers } from './getUsers.ts';
 export { removeUser } from './removeUser.ts';
