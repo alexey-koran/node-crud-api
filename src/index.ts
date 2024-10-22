f284d0d1318436735a8989d50986afc18e71ebb6
Alexey Koran <alexey.koran@gmail.com>
2024-10-22 06:33:25 +0400

    fix: `index.ts` eslint error

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import { env } from 'node:process';
 import { DEFAULT_API_PORT } from './constants/index.ts';
 import { startServer } from './server/index.ts';
 
-const port: number = Number(env.API_PORT) || DEFAULT_API_PORT;
+const apiPort = Number(env.API_PORT);
+
+const port: number = !isNaN(apiPort) ? apiPort : DEFAULT_API_PORT;
 
 startServer(port);
