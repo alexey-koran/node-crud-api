141210a5ae42a51c17cd7c6e740144ac2e400f4a
Alexey Koran <alexey.koran@gmail.com>
2024-10-22 06:14:28 +0400

    fix: `getUsers` missed return type

--- src/db/getUsers.ts
+++ src/db/getUsers.ts
@@ -1,5 +1,5 @@
 import type { Database } from '../types/db.ts';
 
-export const getUsers = (database: Database) => {
+export const getUsers = (database: Database): Database => {
   return structuredClone(database);
 };
